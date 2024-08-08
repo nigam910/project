a210ae6aa0984e211567bbffe572770a30fa63c1
nigam910 <nigamdhillo3021@gmail.com>
2024-08-08 11:17:11 +0530

    Fix apostrophe ESLint errors

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -53,9 +53,9 @@ const Contact = () => {
             {/* from */}
             <div className="xl:w-[54%] order-2 xl:order-none">
               <form className=" flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl ">
-                <h3 className="text-4xl text-accent">Let's work together</h3>
+                <h3 className="text-4xl text-accent">Lets work together</h3>
                 <p className="text-white/60">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi maxime provident ipsum velit porro culpa?</p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi maxime provident ipsum velit porro culpa</p>
                   {/* input  */}
                   <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
                     <Input type="firstname" placeholder="Firstname" />
